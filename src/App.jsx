6cb0c7d8752360af8c94f98574801113c7ebcc84
vickanS <stennvictoria@gmail.com>
2024-05-03 16:54:20 +0200

    a commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,6 @@ import Nav from "./components/Nav"
 import Home from "./pages/Home"
 import Contact from "./pages/Contact"
 import About from './pages/About'
-import Projects from './pages/projects'
-
-
 
 
 function App() {
@@ -23,8 +20,6 @@ function App() {
         <Route index path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/projects" element={<Projects />} />
-
 
       </Routes>
 
