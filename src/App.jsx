eb720ac5ab06128268a2aec382059b3ae51dea5b
vickanS <stennvictoria@gmail.com>
2024-05-03 16:34:58 +0200

    a commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './index.css'
+import React from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Nav from "./components/Nav"
 import Home from "./pages/Home"
 import Contact from "./pages/Contact"
 import About from './pages/About'
-
+import Projects from './pages/projects'
 
 
 
@@ -22,6 +23,7 @@ function App() {
         <Route index path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/projects" element={<Projects />} />
 
 
       </Routes>
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,3 +1,4 @@
+
 import "../styles/Projects.css"
 
 function Projects() {
