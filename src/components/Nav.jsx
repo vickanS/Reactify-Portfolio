426ac39f7aed5bfbd0e93d459d004d09ac31c814
vickanS <stennvictoria@gmail.com>
2024-05-06 17:13:41 +0200

    changed files
    Changes in style and layout. added text in Home, added nav-icon and overlay in Nav.

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,38 +1,40 @@
 import { Link } from "react-router-dom"
+import { useState } from "react"
 import "../styles/Nav.css"
 
 const Nav = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen)
+  }
 
   return (
     <header className="header">
-
-        <h1 className="title">Victorias sida</h1>
-
-        <nav className="nav">
-            <ul className="navlist">
-
-                <Link to={"/"}>
-                  <li className="navitem">Home</li>
-                </Link>
-
-                <Link to={"/about"}>
+      <h1 className="title">Victorias portfolio</h1>
+      <button className="hamburger" onClick={toggleMenu}>☰</button>
+      {isOpen && (
+        <div className="overlay" onClick={toggleMenu}>
+          <nav className="nav">
+          <ul className={`navlist ${isOpen ? 'open' : ''}`}>
+              <Link to={"/"}>
+                <li className="navitem">Home</li>
+              </Link>
+              <Link to={"/about"}>
                 <li className="navitem">About me</li>
-                </Link>
-
-                <Link to={"/projects"}>
+              </Link>
+              <Link to={"/projects"}>
                 <li className="navitem">Projects</li>
-                </Link>
-
-                <Link to={"/contact"}>
+              </Link>
+              <Link to={"/contact"}>
                 <li className="navitem">Contact</li>
-                </Link>
-
+              </Link>
             </ul>
-        </nav>
-
+          </nav>
+        </div>
+      )}
     </header>
-    
-  )
+  );
 }
 
 export default Nav
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,11 @@ import "../styles/Home.css"
 
 const Home = () => {
   return (
-   <section className="homepageheader">
-        <h1 className="welcome">Welcome to my digital portfolio page!</h1>
+   <section className="welcometext">
+        <h1 className="welcome">Welcome to my portfolio page!</h1>
+        <p className="welcomep">Hey! My name is Victoria Sten Åsenius, I'm 26 years old and i live in Kalmar, Sweden.
+        On this page you'll find information about me, my education, my experience with frontend developing but you can
+        also hav a look at som of my projects i have on my Github page.</p>
    </section>
   )
 }
