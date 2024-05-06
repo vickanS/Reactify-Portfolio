c765cc9e4baf894bb287d7571f4a17bef09cd6b8
vickanS <stennvictoria@gmail.com>
2024-05-06 10:30:07 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Nav from "./components/Nav"
 import Home from "./pages/Home"
 import Contact from "./pages/Contact"
 import About from './pages/About'
-
+import Projects from './pages/Projects'
 
 function App() {
 
@@ -20,7 +20,8 @@ function App() {
         <Route index path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
-
+        <Route path="/projects" element={<Projects />} />
+        
       </Routes>
 
     </BrowserRouter>
