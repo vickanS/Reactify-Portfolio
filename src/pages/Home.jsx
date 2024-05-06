6d3e432374ee0a8d66f432167e50fdc9f9030e3d
vickanS <stennvictoria@gmail.com>
2024-05-06 20:00:33 +0200

    Update Home.jsx
    Changed some text.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,9 +4,9 @@ const Home = () => {
   return (
    <section className="welcometext">
         <h1 className="welcome">Welcome to my portfolio page!</h1>
-        <p className="welcomep">Hey! My name is Victoria Sten Åsenius, I'm 26 years old and i live in Kalmar, Sweden.
+        <p className="welcomep">Hey! My name is Victoria Sten Åsenius, I'm 26 years old and I live in Kalmar, Sweden.
         On this page you'll find information about me, my education, my experience with frontend developing but you can
-        also hav a look at som of my projects i have on my Github page.</p>
+        also have a look at som of my projects i have on my Github page.</p>
    </section>
   )
 }
