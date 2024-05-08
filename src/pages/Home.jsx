8313232806c0bca81c21e19ab5c087a23c267606
vickanS <stennvictoria@gmail.com>
2024-05-08 18:04:02 +0200

    Added image
    Added img in the home.jsx file and made some changes in the structure of the page. opdated the styling in Home.css file.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,14 @@ import "../styles/Home.css"
 
 const Home = () => {
   return (
-   <section className="welcometext">
-        <h1 className="welcome">Welcome to my portfolio page!</h1>
-        <p className="welcomep">Hey! My name is Victoria Sten Åsenius, I'm 26 years old and I live in Kalmar, Sweden.
-        On this page you'll find information about me, my education, my experience with frontend developing but you can
-        also have a look at some of my projects i have on my Github page.</p>
+   <section className="welcome-container">
+    <img className="heropic" src="src/assets/Homepic.jpg" alt="Victoria Sten Åsenius" />
+        <div className="welcometext">
+            <h1 className="welcome">Welcome to my portfolio page!</h1>
+            <p className="welcomep">Hey! My name is Victoria Sten Åsenius, I'm 26 years old and I live in Kalmar, Sweden.
+            On this page you'll find information about me, my education, my experience with frontend developing but you can
+            also have a look at some of my projects i have on my Github page.</p>
+        </div>
    </section>
   )
 }
