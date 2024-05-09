e8734b644fa50daed0e069819dad18cd7d41cf88
vickanS <stennvictoria@gmail.com>
2024-05-09 15:44:01 +0200

    Changes and additions
    Added axios dependencies, minor changes in Contact.css and added an API to my github repo in the Projects.jsx file and styled it in the Projects.css file.

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,11 +1,44 @@
-
 import "../styles/Projects.css"
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function Projects() {
+const Projects = () => {
+  const [repos, setRepos] = useState([]); // State för att lagra repositories
+
+  useEffect(() => {
+    axios.get('https://api.github.com/users/vickanS/repos')
+      .then(response => {
+        setRepos(response.data); // Spara data i state
+      })
+      .catch(error => console.error('Error fetching data:', error));
+  }, []); // Tom dependency array för att köra endast vid första render
+
   return (
-    <div>hallåja</div>
-  )
-}
+    <div>
 
-export default Projects
+      <section className="projectsection">
+        <section className="projectshead">
+          <h1 className="projectsheader">My Github projects</h1>
+          <p className="projectsp">This list contains of the various projects I've worked on and uploaded on Github during my studies.</p>
+        </section>
+      </section>
 
+      <section className="projects-container githubprojects">
+        <section className="projectslist">
+          <ul>
+            {repos.map(repo => (
+              <li key={repo.id}>
+                <a href={`https://github.com/vickanS/${repo.name}`} target="_blank" rel="noopener noreferrer">
+                  {repo.name}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </section>
+      </section>
+     
+    </div>
+  ); 
+};
+
+export default Projects;
